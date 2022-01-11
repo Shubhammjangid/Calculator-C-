d16eb7a2b0e5fe40c637042bd761b56c98ae9416
Shubhammjangid <shubham.jangid@netsolutions.com>
2022-01-11 21:25:53 +0530

    Initial commit

---
+++ Function.cs
@@ -0,0 +1,25 @@
+class Calculation{
+    public int Add(int A, int B)  
+    {  
+        return A + B;
+    }  
+    public int Multiply(int A, int B)  
+    {  
+        return A * B;  
+    }  
+    public int Subtract(int A, int B)  
+    {  
+            if (A > B)  
+            {  
+                return A - B;  
+            }  
+  
+            return A - B;  
+
+        }  
+    public float Division(float A, float B)   
+    {  
+        return A / B;  
+    }  
+  
+}  
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Calulator; 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation Op = new Calculation();
+            Console.WriteLine("please enter the Number1");  
+            int A = Convert.ToInt32(Console.ReadLine());  
+            Console.WriteLine("please enter the Number2");  
+            int B = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Choose an option from the following list:");
+            Console.WriteLine("Ad");
+            Console.WriteLine("Sub");
+            Console.WriteLine("Mul");
+            Console.WriteLine("Div");
+            switch (Console.ReadLine())
+    {
+            case "Ad":
+                Console.WriteLine($"The result is:{Op.Add(A,B)}");
+                break;
+            case "Sub":
+                Console.WriteLine($"The result is:{Op.Subtract(A,B)}");
+                break;
+            case "Mul":
+                Console.WriteLine($"The Result is: {Op.Multiply(A,B)}");
+                break;
+            case "Div":
+                Console.WriteLine($"The Result is: {Op.Division(A,B)}");
+                break;
+    }   
+        }
+    }
