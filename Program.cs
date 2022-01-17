700192a682ca743d9c6551662068f6af2fafc226
Shubhammjangid <shubham.jangid@netsolutions.com>
2022-01-17 20:15:03 +0530

    second commit

--- Program.cs
+++ Program.cs
@@ -4,45 +4,66 @@ namespace Calulator;
     {
         static void Main(string[] args)
         {
-            Calculation Op = new Calculation();
-            Console.WriteLine("please enter the Number1");  
-            int A = Convert.ToInt32(Console.ReadLine());  
-            Console.WriteLine("please enter the Number2");  
-            int B = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Choose an option from the following list:");
-            Console.WriteLine("Addition --/ 1 ");    //Addition
-            Console.WriteLine("Subtraction --/ 2");   //Subtraction
-            Console.WriteLine("Multiplication --/ 3");   //Multiplication
-            Console.WriteLine("Division --/ 4");  //Division
-            Console.WriteLine("Square --/ 5");   //square
-            Console.WriteLine("Square root --/ 6");
-            Console.Write("Select one option -- ");
-            switch (Convert.ToInt32(Console.ReadLine()))
-    {
-            case 1:
-                Op.Addition(A,B);
-                break;
-            case 2:
-                Op.Subtraction(A,B);
-                break;
-            case 3:
-                Op.Multiplication(A,B);
-                break;
-            case 4:
-                Op.Division(A,B);
-                break;
-            case 5:
-                Op.Square(A);
-                Op.Square(B);
-                break;
-            case 6:
-                Op.SquareRoot(A);
-                Op.SquareRoot(B);
-                break;
-            default:
-                Console.WriteLine("Enter the proper Number");
-                break;
+            #region MainSystem
 
-    }   
+            try{
+                Calculation Op = new Calculation();
+                Option Ok = new Option();
+                Console.WriteLine("please enter the Number1");  
+                int A = Convert.ToInt32(Console.ReadLine());  
+                Console.WriteLine("-----------------------");
+                Console.WriteLine("please enter the Number2");  
+                int B = Convert.ToInt32(Console.ReadLine());
+
+                bool Value=false;
+
+                while(!Value){
+                        Ok.Optionn();
+                    switch (Convert.ToInt32(Console.ReadLine()))
+            {
+                    case 1:
+                        Op.Addition(A,B);
+                        break;
+
+                    case 2:
+                        Op.Subtraction(A,B);
+                        break;
+
+                    case 3:
+                        Op.Multiplication(A,B);
+                        break;
+
+                    case 4:
+                        Op.Division(A,B);
+                        break;
+
+                    case 5:
+                        Op.Square(A);
+                        Op.Square(B);
+                        break;
+
+                    case 6:
+                        Op.SquareRoot(A);
+                        Op.SquareRoot(B);
+                        break;
+
+                    case 7:
+                        Console.WriteLine("Enter 7 once again to exit from App");
+                        int Exit = Convert.ToInt32(Console.ReadLine());
+                        if(Exit==7){
+                            Value=true;
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("Enter the proper Number");
+                        break;
+            }   
+                }
+            }
+            catch(Exception ex){
+                Console.WriteLine(ex.Message);
+            }
+            #endregion
         }
     }
---
+++ Selection.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Calulator{
+
+    public class Option{
+        public void Optionn(){
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Choose an option from the following list:");
+            Console.WriteLine("Press 1 for Addition ");    
+            Console.WriteLine("Press 2 for Subtraction");   
+            Console.WriteLine("Press 3 for Multiplication");   
+            Console.WriteLine("press 4 for Division"); 
+            Console.WriteLine("press 5 for square ");  
+            Console.WriteLine("Press 6 for Square root");
+            Console.WriteLine("Press 7 to exit");
+            Console.WriteLine("---------------------------");
+            Console.Write("Select one option -- ");
+        }
+    }
+}
